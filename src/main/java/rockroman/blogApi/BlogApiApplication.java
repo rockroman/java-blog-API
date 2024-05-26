122f095819b37147dd44bf39a0ac27dcbbe921fc
Roman Rakic <rock.rakic@gmail.com>
2024-05-26 01:01:56 +0100

    set port var to see what env to load

--- src/main/java/rockroman/blogApi/BlogApiApplication.java
+++ src/main/java/rockroman/blogApi/BlogApiApplication.java
@@ -9,11 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
-		SpringApplication.run(BlogApiApplication.class, args);
+		// Check if environment variables are available from Heroku
+		String port = System.getenv("PORT");
+		if (port != null) {
+			// Environment variables are available from Heroku, no need to load .env file
+			SpringApplication.run(BlogApiApplication.class, args);
+		} else {
+			// Load environment variables from .env file
+			Dotenv dotenv = Dotenv.configure().load();
+			dotenv.entries().forEach(entry ->
+					System.setProperty(entry.getKey(), entry.getValue())
+			);
+			SpringApplication.run(BlogApiApplication.class, args);
+		}
 	}
 
 }
