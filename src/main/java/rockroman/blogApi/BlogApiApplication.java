dee4bb28f110ddb8d468c02a6f77fca1b5e0ce56
Roman Rakic <rock.rakic@gmail.com>
2024-05-30 08:48:18 +0100

    fix:remove property annotation so .env file can be loaded

--- src/main/java/rockroman/blogApi/BlogApiApplication.java
+++ src/main/java/rockroman/blogApi/BlogApiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:${user.dir}/.env")
 public class BlogApiApplication {
 
 	public static void main(String[] args) {
